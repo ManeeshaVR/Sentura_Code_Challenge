331d479255d33d624a2437294324ef364cc0f5b2
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-08-25 12:53:02 +0530

    Add URL & Key

--- src/main/java/dev/vimukthi/sentura_interview/controller/UserController.java
+++ src/main/java/dev/vimukthi/sentura_interview/controller/UserController.java
@@ -10,12 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/user")
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 
     private final WeavyClient weavyClient;
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    private static final String url = "https://a06dd3674d1440a781890212e1079a7e.weavy.io/api/users";
+    private static final String key = "wys_LqU4ONwDc8khDBApHoliNG69x70iC21MeSSO";
 
     @GetMapping("/check")
     public String checkTest() {
@@ -23,9 +25,9 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<?> getAllCustomers() {
+    public ResponseEntity<?> getAlLUsers() {
         try {
-            String response = weavyClient.getWeavyAllUserData("https://a06dd3674d1440a781890212e1079a7e.weavy.io/api/users", "wys_LqU4ONwDc8khDBApHoliNG69x70iC21MeSSO");
+            String response = weavyClient.getWeavyAllUserData(url, key);
             logger.info("Fetching all users");
             logger.info("Response: " + response);
             return ResponseEntity.ok(response);
@@ -36,9 +38,9 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getCustomerById(@PathVariable String id) {
+    public ResponseEntity<?> getUserById(@PathVariable String id) {
         try {
-            return ResponseEntity.ok(weavyClient.getWeavyUserData("https://a06dd3674d1440a781890212e1079a7e.weavy.io/api/users/"+ id, "wys_LqU4ONwDc8khDBApHoliNG69x70iC21MeSSO"));
+            return ResponseEntity.ok(weavyClient.getWeavyUserData(url+ id, key));
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Internal server error | Unable to fetch users.\nMore Details\n" + exception);
@@ -48,7 +50,7 @@ public class UserController {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> saveUser(@RequestBody UserDTO userDTO) {
         try {
-            return ResponseEntity.ok(weavyClient.postWeavyData("https://a06dd3674d1440a781890212e1079a7e.weavy.io/api/users", "wys_LqU4ONwDc8khDBApHoliNG69x70iC21MeSSO", userDTO.toString()));
+            return ResponseEntity.ok(weavyClient.postWeavyData(url, key, userDTO.toString()));
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Internal server error | Unable to fetch users.\nMore Details\n" + exception);
@@ -58,7 +60,7 @@ public class UserController {
     @PutMapping("/{id}")
     public ResponseEntity<?> updateUser(@PathVariable String id, @RequestBody UserDTO userDTO) {
         try {
-            return ResponseEntity.ok(weavyClient.putWeavyData("https://a06dd3674d1440a781890212e1079a7e.weavy.io/api/users/"+ id, "wys_LqU4ONwDc8khDBApHoliNG69x70iC21MeSSO", userDTO.toString()));
+            return ResponseEntity.ok(weavyClient.putWeavyData(url+ id, key, userDTO.toString()));
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Internal server error | Unable to fetch users.\nMore Details\n" + exception);
@@ -68,7 +70,7 @@ public class UserController {
     @PostMapping("/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable String id) {
         try {
-            return ResponseEntity.ok(weavyClient.deleteWeavyData("https://a06dd3674d1440a781890212e1079a7e.weavy.io/api/users/"+id+"/trash", "wys_LqU4ONwDc8khDBApHoliNG69x70iC21MeSSO"));
+            return ResponseEntity.ok(weavyClient.deleteWeavyData(url+id+"/trash", key));
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Internal server error | Unable to fetch users.\nMore Details\n" + exception);
