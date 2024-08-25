bca1728dc8a0741b219612ef340ef52828a3687d
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-08-25 10:31:23 +0530

    Finalize

--- src/main/java/dev/vimukthi/sentura_interview/controller/UserController.java
+++ src/main/java/dev/vimukthi/sentura_interview/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/ser")
+@RequestMapping("api/user")
 @RequiredArgsConstructor
 public class UserController {
 
