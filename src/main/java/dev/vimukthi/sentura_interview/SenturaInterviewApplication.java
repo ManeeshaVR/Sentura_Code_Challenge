70ffb903346ad7b07127dc2e3bcaaabc96b54a4f
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-08-25 09:00:33 +0530

    Initi Project

---
+++ src/main/java/dev/vimukthi/sentura_interview/SenturaInterviewApplication.java
@@ -0,0 +1,13 @@
+package dev.vimukthi.sentura_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenturaInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenturaInterviewApplication.class, args);
+	}
+
+}
